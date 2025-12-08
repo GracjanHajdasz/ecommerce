df17c8ddbbce5fa1fd3fda1313d6a3ec690ddc47
Gracjan Hajdasz <b3nny3@wp.pl>
2025-12-08 16:43:20 +0100

    using data from backend to generate html

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,17 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import "./HomePage.css";
 import { Header } from "../components/Header";
-import { products } from "../../starting-code/data/products";
 
 export function HomePage() {
-  axios.get("http://localhost:3000/api/products").then((response) => {
-    console.log(response.data);
-  });
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
+
   return (
     <>
       <title>Ecommerce Project</title>
@@ -21,7 +26,9 @@ export function HomePage() {
                   <img className="product-image" src={product.image} />
                 </div>
 
-                <div className="product-name limit-text-to-2-lines">{name}</div>
+                <div className="product-name limit-text-to-2-lines">
+                  {product.name}
+                </div>
 
                 <div className="product-rating-container">
                   <img
