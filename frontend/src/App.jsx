6b66c3e47c1c24c3901d6d98322a6556ea2874f7
Gracjan Hajdasz <b3nny3@wp.pl>
2025-12-08 13:31:36 +0100

    react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
+import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route inmdex element={<HomePage />} />
+      <Route path="checkout" element={<div>test</div>} />
+    </Routes>
   )
 }
 
