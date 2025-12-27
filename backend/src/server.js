fa45a5a74d3818afa99103c688929fc2b8fce579
Gracjan Hajdasz <b3nny3@wp.pl>
2025-12-27 23:52:16 +0100

    test request in API

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/hello', (req, res) => {
+    res.json({message: 'hello world'});
+})
+
 const PORT = 5001;
 server = app.listen(PORT, () => {
     console.log(`server running on PORT ${PORT}`);
